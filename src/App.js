4b0c00527b124394db9e9fbc1b34897c0686af25
Monika Jassal <monikajassal@cqlsys.co.uk>
2021-08-17 18:31:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
-// import Data from "./components/Data";
+import Data from "./components/Data";
 import DataFetchingTwo from "./components/DataFetchingTwo";
+import RefHooks from "./components/RefHooks";
+import DocTitleOne from "./components/DocTitleOne";
+import DocTitleTwo from "./components/DocTitleTwo";
 
 function App() {
   return (
     <div className="App">
       <DataFetchingTwo />
+      <Data />
+      <RefHooks />
+      <DocTitleOne />
+      <DocTitleTwo />
     </div>
   );
 }
---
+++ src/components/RefHooks.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef } from "react";
+
+function useReff() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" />
+    </div>
+  );
+}
+
+export default useReff;
