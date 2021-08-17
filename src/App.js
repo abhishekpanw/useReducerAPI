28c33d0f11e2026f57bde856f21a4bd410832829
Monika Jassal <monikajassal@cqlsys.co.uk>
2021-08-17 16:09:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import Data from "./components/Data";
+import DataFetchingTwo from "./components/DataFetchingTwo";
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetchingTwo />
+    </div>
+  );
+}
+
+export default App;
